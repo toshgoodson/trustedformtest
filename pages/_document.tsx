25f0539af67416b5907333aba6787e08a9214841
Tosh Goodson <tosh.goodson@gmail.com>
2021-12-06 13:04:21 -0800

    remove custom doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,16 +27,4 @@ export default class MyDocument extends Document {
 			sheet.seal()
 		}
 	}
-
-	render() {
-		return (
-			<Html lang="en">
-				<Head/>
-				<body>
-					<Main/>
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
 }
